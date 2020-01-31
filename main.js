ea191f702cbde7b398b6cbe4670200c7f7cf4e99
Lee Gyu Won <joomint99@naver.com>
2020-02-01 04:15:22 +0900

    5주차 express도 끄읕

--- main.js
+++ main.js
@@ -3,92 +3,101 @@ var app = express()
 var fs = require('fs');
 var path = require('path');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
 var sanitizeHtml = require('sanitize-html');
+var compression = require('compression')
 var template = require('./lib/template.js');
- 
+var helmet = require('helmet')
+app.use(helmet());
+
+app.use(express.static('public'));
+//public디렉토리 안에서 static파일을 찾겠다
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+
+app.get('/topic/create', function(request, response){
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/topic/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
+});
+
+
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    ); 
-    response.send(html);
-  });
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+    `
+    <h2>${title}</h2>${description}
+    <img src="/images/hello.png" style="width:300px; display:block; margin-top:10px;">
+    `,
+    `<a href="/topic/create">create</a>`
+  ); 
+  response.send(html);
 });
- 
-app.get('/page/:pageId', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+app.get('/topic/:pageId', function(request, response, next) { 
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    if(err){
+      next(err);
+    } else {
       var title = request.params.pageId;
       var sanitizedTitle = sanitizeHtml(title);
       var sanitizedDescription = sanitizeHtml(description, {
         allowedTags:['h1']
       });
-      var list = template.list(filelist);
+      var list = template.list(request.list);
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+        ` <a href="/topic/create/">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
+          <form action="/topic/:pageID/delete_process/" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
       );
       response.send(html);
-    });
+    }
   });
 });
  
-app.get('/create', function(request, response){
-  fs.readdir('./data', function(error, filelist){
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
-});
- 
-app.post('/create_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
+app.post('/topic/create_process', function(request, response){
+      var post = request.body;
       var title = post.title;
       var description = post.description;
       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `page/${title}/`});
+        response.writeHead(302, {Location: `/topic/${title}/`});
         response.end();
       })
   });
-});
- 
-app.get('/update/:pageId', function(request, response){
-  fs.readdir('./data', function(error, filelist){
+
+app.get('/topic/update/:pageId', function(request, response){
     var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
       var title = request.params.pageId;
-      var list = template.list(filelist);
+      var list = template.list(request.list);
       var html = template.HTML(title, list,
         `
-        <form action="/update_process" method="post">
+        <form action="/topic/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p>
@@ -99,40 +108,30 @@ app.get('/update/:pageId', function(request, response){
           </p>
         </form>
         `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
       );
       //위의 코드에서 /update?id=${title} 부분은 /update/${title}로 수정 되어야 하는 버그입니다. 
       response.send(html);
     });
-  });
 });
  
-app.post('/update_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
+app.post('/topic/update_process', function(request, response){
+  
+      var post = request.body;
       var id = post.id;
       var title = post.title;
       var description = post.description;
       fs.rename(`data/${id}`, `data/${title}`, function(error){
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/page/${title}`});
+          response.writeHead(302, {Location: `/topic/${title}`});
           response.end();
         })
       });
-  });
 });
  
-app.post('/:pageID/delete_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
+app.post('/topic/:pageID/delete_process/', function(request, response){
+  
+      var post = request.body;
       var id = post.id;
       var filteredId = path.parse(id).base;
       fs.unlink(`data/${filteredId}`, function(error){
@@ -140,12 +139,20 @@ app.post('/:pageID/delete_process', function(request, response){
         response.end();
       })
   });
+
+app.use(function(req,res,next){
+  response.status(404).send('Sorry cannot find that! Gyuwon write');
 });
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 });
+
 /*
 
 var http = require('http');
